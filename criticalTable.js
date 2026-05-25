8f566bc857fcc31bc635014fc238f0489a716a5c
Sparkston <sparkston@hotmail.com>
2026-05-25 15:35:49 +0200

    Add files via upload

---
+++ criticalTable.js
@@ -0,0 +1,303 @@
+module.exports = {
+  11: {
+    injury: "Winded",
+    fatal: false,
+    timeLimit: null,
+    effects: "None.",
+    healing: null
+  },
+
+  12: {
+    injury: "Stunned",
+    fatal: false,
+    timeLimit: null,
+    effects: "None.",
+    healing: null
+  },
+
+  13: {
+    injury: "Crippling pain",
+    fatal: false,
+    timeLimit: null,
+    effects: "Stress Level increases one step.",
+    healing: null
+  },
+
+  14: {
+    injury: "Sprained ankle",
+    fatal: false,
+    timeLimit: null,
+    effects: "MOBILITY -2 and cannot run until a MEDICAL AID roll is made.",
+    healing: null
+  },
+
+  15: {
+    injury: "Blood in eyes",
+    fatal: false,
+    timeLimit: null,
+    effects: "OBSERVATION and RANGED COMBAT -2 until a MEDICAL AID roll is made.",
+    healing: null
+  },
+
+  16: {
+    injury: "Concussion",
+    fatal: false,
+    timeLimit: null,
+    effects: "MOBILITY -2.",
+    healing: "D6 days"
+  },
+
+  21: {
+    injury: "Severed ear",
+    fatal: false,
+    timeLimit: null,
+    effects: "OBSERVATION -2.",
+    healing: "D6 days"
+  },
+
+  22: {
+    injury: "Broken toes",
+    fatal: false,
+    timeLimit: null,
+    effects: "Running becomes a slow action.",
+    healing: "D6 days"
+  },
+
+  23: {
+    injury: "Broken hand",
+    fatal: false,
+    timeLimit: null,
+    effects: "Cannot use hand.",
+    healing: "D6 days"
+  },
+
+  24: {
+    injury: "Knocked out teeth",
+    fatal: false,
+    timeLimit: null,
+    effects: "MANIPULATION -2.",
+    healing: "D6 days"
+  },
+
+  25: {
+    injury: "Impaled thigh",
+    fatal: false,
+    timeLimit: null,
+    effects: "Running becomes a slow action.",
+    healing: "2D6 days"
+  },
+
+  26: {
+    injury: "Slashed shoulder",
+    fatal: false,
+    timeLimit: null,
+    effects: "Cannot use arm.",
+    healing: "D6 days"
+  },
+
+  31: {
+    injury: "Broken nose",
+    fatal: false,
+    timeLimit: null,
+    effects: "MANIPULATION and OBSERVATION -1.",
+    healing: "D6 days"
+  },
+
+  32: {
+    injury: "Crotch hit",
+    fatal: false,
+    timeLimit: null,
+    effects: "Take 1 damage whenever rolling MOBILITY or CLOSE COMBAT.",
+    healing: "D6 days"
+  },
+
+  33: {
+    injury: "Broken ribs",
+    fatal: false,
+    timeLimit: null,
+    effects: "MOBILITY and CLOSE COMBAT -2.",
+    healing: "2D6 days"
+  },
+
+  34: {
+    injury: "Gouged eye",
+    fatal: false,
+    timeLimit: null,
+    effects: "RANGED COMBAT and OBSERVATION -2.",
+    healing: "2D6 days"
+  },
+
+  35: {
+    injury: "Busted kneecap",
+    fatal: false,
+    timeLimit: null,
+    effects: "Cannot run, only crawl.",
+    healing: "2D6 days"
+  },
+
+  36: {
+    injury: "Broken arm",
+    fatal: false,
+    timeLimit: null,
+    effects: "Cannot use arm.",
+    healing: "2D6 days"
+  },
+
+  41: {
+    injury: "Broken leg",
+    fatal: false,
+    timeLimit: null,
+    effects: "Cannot run, only crawl.",
+    healing: "2D6 days"
+  },
+
+  42: {
+    injury: "Crushed foot",
+    fatal: false,
+    timeLimit: null,
+    effects: "Cannot run, only crawl.",
+    healing: "3D6 days"
+  },
+
+  43: {
+    injury: "Crushed elbow",
+    fatal: false,
+    timeLimit: null,
+    effects: "Cannot use arm.",
+    healing: "3D6 days"
+  },
+
+  44: {
+    injury: "Punctured lung",
+    fatal: true,
+    fatalModifier: 0,
+    timeLimit: "One Day",
+    effects: "STAMINA and MOBILITY -2.",
+    healing: "D6 days"
+  },
+
+  45: {
+    injury: "Bleeding gut",
+    fatal: true,
+    fatalModifier: 0,
+    timeLimit: "One Shift",
+    effects: "Take 1 damage whenever rolling MOBILITY or CLOSE COMBAT.",
+    healing: "D6 days"
+  },
+
+  46: {
+    injury: "Ruptured intestines",
+    fatal: true,
+    fatalModifier: 0,
+    timeLimit: "One Shift",
+    effects: "Disease (Virulence 6).",
+    healing: "2D6 days"
+  },
+
+  51: {
+    injury: "Busted kidney",
+    fatal: true,
+    fatalModifier: 0,
+    timeLimit: "One Day",
+    effects: "Cannot run, only crawl. MOBILITY -2.",
+    healing: "2D6 days"
+  },
+
+  52: {
+    injury: "Arm artery cut",
+    fatal: true,
+    fatalModifier: -1,
+    timeLimit: "One Turn",
+    effects: "Cannot use arm.",
+    healing: "D6 days"
+  },
+
+  53: {
+    injury: "Leg artery cut",
+    fatal: true,
+    fatalModifier: -1,
+    timeLimit: "One Turn",
+    effects: "Running becomes a slow action.",
+    healing: "D6 days"
+  },
+
+  54: {
+    injury: "Severed arm",
+    fatal: true,
+    fatalModifier: -1,
+    timeLimit: "One Shift",
+    effects: "Cannot use arm.",
+    healing: "Permanent"
+  },
+
+  55: {
+    injury: "Severed leg",
+    fatal: true,
+    fatalModifier: -1,
+    timeLimit: "One Shift",
+    effects: "Cannot run, only crawl.",
+    healing: "Permanent"
+  },
+
+  56: {
+    injury: "Cracked spine",
+    fatal: false,
+    timeLimit: null,
+    effects: "Paralyzed from the neck down. Permanent if not treated in time.",
+    healing: "3D6 days"
+  },
+
+  61: {
+    injury: "Ruptured jugular",
+    fatal: true,
+    fatalModifier: -1,
+    timeLimit: "One Round",
+    effects: "STAMINA -1.",
+    healing: "2D6 days"
+  },
+
+  62: {
+    injury: "Ruptured aorta",
+    fatal: true,
+    fatalModifier: -2,
+    timeLimit: "One Round",
+    effects: "STAMINA -2.",
+    healing: "3D6 days"
+  },
+
+  63: {
+    injury: "Disemboweled",
+    fatal: true,
+    fatalModifier: null,
+    timeLimit: null,
+    effects: "Instant death.",
+    healing: null
+  },
+
+  64: {
+    injury: "Crushed skull",
+    fatal: true,
+    fatalModifier: null,
+    timeLimit: null,
+    effects: "Your story ends here.",
+    healing: null
+  },
+
+  65: {
+    injury: "Pierced head",
+    fatal: true,
+    fatalModifier: null,
+    timeLimit: null,
+    effects: "You die immediately.",
+    healing: null
+  },
+
+  66: {
+    injury: "Impaled heart",
+    fatal: true,
+    fatalModifier: null,
+    timeLimit: null,
+    effects: "Your heart beats for the last time.",
+    healing: null
+  }
+};
