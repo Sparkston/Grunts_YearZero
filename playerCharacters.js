7812550745e96b04c36d034550ff015f55db33da
Sparkston <sparkston@hotmail.com>
2026-05-27 19:29:55 +0200

    Add files via upload

---
+++ playerCharacters.js
@@ -0,0 +1,21 @@
+module.exports = {
+  PC1: {
+    name: "Sgt. Michael T Willis",
+    callsign: "Willis"
+  },
+
+  PC2: {
+    name: "Sgt Pierre Nguyen Nguyen",
+    callsign: "Nguyen"
+  },
+
+  PC3: {
+    name: "PFC Jonah \"Holy Jo\" Bruckmeir",
+    callsign: "Holy Jo"
+  },
+
+  PC4: {
+    name: "Specialist Dauncey \"Greasegun\" Duke",
+    callsign: "Greasegun"
+  }
+};
